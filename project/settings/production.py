92e9255d1b1245ab27fd59f6f74d2a1dfb9f38f2
0sw4l <ioswxd@gmail.com>
2018-11-19 05:29:42 -0500

    fix deploy

--- project/settings/production.py
+++ project/settings/production.py
@@ -28,10 +28,10 @@ WSGI_APPLICATION = 'project.wsgi.production.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',
+        'NAME': 'd6s6p0287p4fdj',
+        'USER': 'zjjfbbesdkmtey',
+        'PASSWORD': '0304e56e83e850ef0f7f2d4512853e4a26e305eb3d6320e1045daef5d6aa2700',
+        'HOST': 'ec2-50-19-249-121.compute-1.amazonaws.com',
         'PORT': '5432',
     }
 }
